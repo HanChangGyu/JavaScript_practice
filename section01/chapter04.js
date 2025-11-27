3c24459d3546451fa207ac3bc166eaf39fe1f156
HanChangGyu <ceu0723@gmail.com>
2025-11-27 20:37:32 +0900

    first commit: 자바스크립트 기초 실습

---
+++ section01/chapter04.js
@@ -0,0 +1,28 @@
+// 1. 변수
+let age = 27;
+
+age = 30;
+
+// 2. 상수
+const birth = "1997.01.07";
+
+// 3. 변수 명명규칙 (네이밍 규칙)
+// 3-1 $, _ 제외한 기호는 사용할 수 없다.
+let $_name;
+
+// 3-2. 숫자로 시작할 수 없다.
+// let 1name 이런식은 사용 불가
+
+// 3-3. 예약어를 사용할 수 없다.
+
+// 4. 변수 명명 가이드
+/*
+let b = 1;
+let c = a - b;
+let a = 1;
+*/
+
+// 4-1. 
+let salesCount = 1;
+let rfundCount = 1;
+let totalSalesCount = a - b;
