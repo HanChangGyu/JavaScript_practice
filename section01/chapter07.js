f713594c097f21d798006b438ce5e3355b4b0dea
HanChangGyu <ceu0723@gmail.com>
2025-11-29 20:04:01 +0900

    feat: 연산자 추가

---
+++ section01/chapter07.js
@@ -0,0 +1,36 @@
+// 1. 대입 연산자
+let var1 = 1;
+
+// 2. 산술 연산자
+let num1 = 3 + 2;
+let num2 = 3 - 2;
+let num3 = 3 * 2;
+let num4 = 3 / 2;
+let num5 = 3 % 2;
+
+let num6 = 1 + 2 * 10; // 21 - 수학과 같은 순서
+
+// 3. 복합 대입 연산자
+let num7 = 10;
+num7 += 20; // 30
+num7 -= 20; // 10
+num7 *= 20;
+num7 %= 10;
+
+// 4. 증감 연산자
+let num8 = 10;
+++num8 // 11 전위연산
+num8 ++ // 10 후위연산
+
+// 5. 논리 연산자
+let or = true || false;
+
+let and = true && false;
+
+let not = !true;
+
+// 6. 비교 연산자
+let comp1 = 1 === 2; // false
+let comp2 = 1 !== 2; // true
+
+let comp3 = 2 > 1;
