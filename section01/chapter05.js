739256db59ae4c0da3609a62bd3493e0886d1fab
HanChangGyu <ceu0723@gmail.com>
2025-11-29 19:11:19 +0900

    feat: 자바스크립트 기초 문법 Type

---
+++ section01/chapter05.js
@@ -0,0 +1,28 @@
+// 1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+
+// 2. String Type(템플릿 리터럴 문법)
+let myName = "이정환";
+let myLocation = "목동";
+let introduce = myName + myLocation;
+
+// 이정환 + 목동
+
+
+let introduceText = `${myName}은 ${myLocation}에 거주합니다`; // 이정환은 목동에 거주합니다. 
+// 3. Booleane Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다)
+let empty = null;
+
+// 5. Undefind Type (변수를 선언하고 변수에 어떠한 값도 집어넣지 않을때 자동으로 할당)
+let none;
