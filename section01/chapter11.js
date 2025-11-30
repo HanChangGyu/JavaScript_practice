fe8dc47acd18651b42d52382eb9630f1d16e3acd
HanChangGyu <ceu0723@gmail.com>
2025-11-30 16:53:07 +0900

    feat: 함수와 함수 표현식

---
+++ section01/chapter11.js
@@ -0,0 +1,13 @@
+// 함수 선언
+
+function getAree (width, height) { // 전달된 인수를 순서대로 저장하는 매개변수
+    let area = width * height;
+
+    console.log(area);
+}
+
+getAree(10, 20); // 함수에게 전달한 값 인수
+getAree(30, 20);
+
+// 호이스팅
+// -> 끌어올리다 라는 뜻
